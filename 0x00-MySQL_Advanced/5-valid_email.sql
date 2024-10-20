ea3777d040c45447e2d81a412b0da8cc8036f54e
Sesh-Gallagher <t.ntwana@gmail.com>
2024-10-20 02:08:22 +0000

    MySQL Advanced

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,15 +1,17 @@
 -- creates a trigger that resets the attribute valid_email
 -- only when the email has been changed
 
+DROP TRIGGER IF EXISTS validate_email;
 DELIMITER $$
-CREATE TRIGGER email_trigger
+CREATE TRIGGER validate_email
 BEFORE UPDATE ON users
 FOR EACH ROW
 BEGIN
-IF NEW.email <> OLD.email
+    IF OLD.email != NEW.email
 THEN
     SET NEW.valid_email = 0;
-END IF 
-END
-$$
+ELSE
+    SET NEW.valid_email = NEW.valid_email;
+END IF;
+END $$
 DELIMITER ;
